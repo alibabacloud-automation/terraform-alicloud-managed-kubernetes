b0253b1068d6dfc66e5b1a1bae2c8198e0bf2d8a
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-01-11 13:57:11 +0100

    Fix: variable name
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "alicloud_cs_managed_kubernetes" "this" {
 
 
   dynamic "maintenance_window" {
-    for_each = var.maintenance_window.value.enabled ? [1]: []
+    for_each = var.maintenance_window.enabled ? [1]: []
 
     content {
       enable            = maintenance_window.value.enabled
