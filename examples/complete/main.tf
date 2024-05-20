57279895f716c0163ad84e3b5d48e2797032c2ee
shanye <shanye.xzq@alibaba-inc.com>
2024-05-20 17:39:21 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -14,14 +14,24 @@ resource "alicloud_vswitch" "default" {
   zone_id      = data.alicloud_zones.default.zones.0.id
 }
 
+data "alicloud_instance_types" "cloud_essd" {
+  availability_zone    = data.alicloud_zones.default.zones.0.id
+  cpu_core_count       = 4
+  memory_size          = 8
+  kubernetes_node_role = "Worker"
+  system_disk_category = "cloud_essd"
+}
+
 module "managed-k8s" {
-  source             = "../../"
-  k8s_name_prefix    = "tf-example"
-  cluster_spec       = "ack.pro.small"
-  vswitch_ids        = [alicloud_vswitch.default.id]
-  k8s_pod_cidr       = cidrsubnet("10.0.0.0/8", 8, 36)
-  k8s_service_cidr   = cidrsubnet("172.16.0.0/16", 4, 7)
-  kubernetes_version = "1.24.6-aliyun.1"
+  source                = "../../"
+  k8s_name_prefix       = "tf-example"
+  cluster_spec          = "ack.pro.small"
+  vswitch_ids           = [alicloud_vswitch.default.id]
+  k8s_pod_cidr          = cidrsubnet("10.0.0.0/8", 8, 36)
+  k8s_service_cidr      = cidrsubnet("172.16.0.0/16", 4, 7)
+  kubernetes_version    = "1.24.6-aliyun.1"
+  worker_instance_types = [data.alicloud_instance_types.cloud_essd.instance_types.0.id]
+  worker_disk_category  = "cloud_essd"
   cluster_addons = [
     {
       name   = "flannel",
